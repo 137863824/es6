8833d2b9896b7e1b19408fe883bcb18f536c02b0
cak <137863824@qq.com>
2017-12-13 15:59:20 +0800

    0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # es6
 代码笔记
+# .babelrc babel转码器配置文件（配置项，类似package.json）
+转码命令：npm run build
---
+++ letconst.js
@@ -0,0 +1,23 @@
+for (let i = 0; i < 3; i++) { // i为父作用域
+    let i = 'abc';   // i为子作用域
+    console.log(i);
+}
+// abc
+// abc
+// abc
+let a = [];
+for (let i = 0; i < 10; i++) {
+  a[i] = function () {
+    console.log(i);
+  };
+}
+a[6](); // 6
+
+let s = 10
+{
+    let s = 1
+    console.log(s)  // 1
+}
+console.log(s)  // 10
+
+// let: 1、同一个作用域内不能多次定义。2、作用域限制（暂时性死区）。3、先定义后使用（禁止变量提升）
